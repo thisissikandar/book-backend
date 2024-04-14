24d39abd9d6bf98f3ce1f9954c0c1ae098083c2f
thisissikandar <sikandarc989@gmail.com>
2024-04-14 17:33:42 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import createHttpError, { HttpError } from "http-errors";
+import  globalErrorHandle from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res) => {
+  const error = createHttpError(400, "something went wrong");
+  throw error;
   res.json({ messagee: "hello world server is listening" });
 });
+
+app.use(globalErrorHandle);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ dotenv.config()
 const _config = {
   port: process.env.APP_PORT,
   databaseUrl: process.env.MONGO_URI,
+  env: process.env.NODE_ENV
 };
 
 export const config = Object.freeze(_config);
